7de1dbc4da45343faa4eb14322bcb2ad83f9695e
prodrigues1990@gmail.com <prodrigues1990@gmail.com@2999aafc-b4f9-c9b5-0511-6611d126045f>
2013-07-31 01:20:40 +0000

    Fixed Blip object list not being updated on ForceVehicle-SpawnVehicle. Issue 4.

--- trunk/IVVehicleSaver/Properties/AssemblyInfo.cs
+++ trunk/IVVehicleSaver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GTA IV script_name")]
+[assembly: AssemblyTitle("IVVehicleSaver")]
 [assembly: AssemblyDescription("C# Script for GTA IV")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GTA IV script_name")]
+[assembly: AssemblyProduct("IVVehicleSaver")]
 [assembly: AssemblyCopyright("Public Domain Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.7.*")]
+[assembly: AssemblyVersion("0.1.13.*")]
